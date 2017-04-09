d61ed13e83350d443cd0701828287051d7ac67d7
red <red@red-pc.lan>
2017-04-09 16:05:25 +1000

    fixed all paths to be relative to documents and not server root, added bootstrap classes to necessary inputs, tidied up english strings, polished config demonstration

--- js/utility.js
+++ js/utility.js
@@ -1,43 +1,43 @@
 /* Various page processing stuff for CFGen
 https://mkrl.github.io/cfgen/ */
 
-		
 
-		
-		
+
+
+
 		function autoFillGood() {											//automatically fill network settings
-            document.getElementById('cmdrate').value = "67";   
-            document.getElementById('interp').value = ".0152";   
-            document.getElementById('intratio').value = "1";   
-            document.getElementById('uprate').value = "67";   
-            document.getElementById('rate').value = "60000";   
+            document.getElementById('cmdrate').value = "67";
+            document.getElementById('interp').value = ".0152";
+            document.getElementById('intratio').value = "1";
+            document.getElementById('uprate').value = "67";
+            document.getElementById('rate').value = "60000";
         }
 		function autoFillBad() {
-            document.getElementById('cmdrate').value = "40";   
-            document.getElementById('interp').value = "0";   
-            document.getElementById('intratio').value = "2";   
-            document.getElementById('uprate').value = "40";   
-            document.getElementById('rate').value = "35000";   
+            document.getElementById('cmdrate').value = "40";
+            document.getElementById('interp').value = "0";
+            document.getElementById('intratio').value = "2";
+            document.getElementById('uprate').value = "40";
+            document.getElementById('rate').value = "35000";
         }
 		function autoFillComp() {
-            document.getElementById('cmdrate').value = "128";   
-            document.getElementById('interp').value = ".007";   
-            document.getElementById('intratio').value = "1";   
-            document.getElementById('uprate').value = "128";   
-            document.getElementById('rate').value = "90000";   
+            document.getElementById('cmdrate').value = "128";
+            document.getElementById('interp').value = ".007";
+            document.getElementById('intratio').value = "1";
+            document.getElementById('uprate').value = "128";
+            document.getElementById('rate').value = "90000";
         }
-		
+
 		function updateInput(ish){													//updating FOV on user default change
 			document.getElementById("v_fov").value = ish;
 		}
-				
-		
-		
+
+
+
 
        function isNumberKey(evt) 					//allowing numbers and decimal points
        {
           var charCode = (evt.which) ? evt.which : evt.keyCode;
-          if (charCode != 46 && charCode > 31 
+          if (charCode != 46 && charCode > 31
             && (charCode < 48 || charCode > 57))
              return false;
 
@@ -47,18 +47,34 @@ https://mkrl.github.io/cfgen/ */
 		function updatedxlevel()
 		{
 
-			if(document.getElementById('toaster').checked) {
+			// if(document.getElementById('toaster').checked) {
+			// dxlevel = "81";
+			// }else if(document.getElementById('stable').checked) {
+			// dxlevel = "81";
+			// }else if(document.getElementById('fine').checked) {
+			// dxlevel = "91";
+			// }else if(document.getElementById('fancy')) {
+			// dxlevel = "98";
+			// }
+
+			// reffering by index is gross
+			var gfxsel = document.getElementById("gfx_selector");
+			if(gfxsel.selectedIndex == 0) {
 			dxlevel = "81";
-			}else if(document.getElementById('stable').checked) {
+			}else if(gfxsel.selectedIndex == 1) { // toaster
 			dxlevel = "81";
-			}else if(document.getElementById('fine').checked) {
+			}else if(gfxsel.selectedIndex == 2) { // stable
+			dxlevel = "81";
+			}else if(gfxsel.selectedIndex == 3) { // fine
 			dxlevel = "91";
-			}else if(document.getElementById('fancy').checked) {
+			}else if(gfxsel.selectedIndex == 4) { // fancy
 			dxlevel = "98";
 			}
-			document.getElementById('loptions').value = "-dxlevel " + dxlevel + " " + screenmod +" -w " + screen.width + " -h " + screen.height + " -console -noborder -novid"; 
+
+
+			document.getElementById('loptions').value = "-dxlevel " + dxlevel + " " + screenmod +" -w " + screen.width + " -h " + screen.height + " -console -noborder -novid";
 		}
-		var screenmod = "-full";	
+		var screenmod = "-full";
 		function updatescreenmode()									//updating screen mode
 		{
 
@@ -67,10 +83,9 @@ https://mkrl.github.io/cfgen/ */
 			}else if(document.getElementById('windowed').checked) {
 			screenmod = "-sw";
 			}
-			document.getElementById('loptions').value = "-dxlevel " + dxlevel + " " + screenmod +" -w " + screen.width + " -h " + screen.height + " -console -noborder -novid"; 
+			document.getElementById('loptions').value = "-dxlevel " + dxlevel + " " + screenmod +" -w " + screen.width + " -h " + screen.height + " -console -noborder -novid";
 		}
-		
+
 		$(document).ready(function(){											//tooltips
-		$('[data-toggle="tooltip"]').tooltip();   
+		$('[data-toggle="tooltip"]').tooltip();
 		});
-	
