6d0586efbb8a5afa141d9b6a8c266753a82d969f
Mikhail <tradecontent@gmail.com>
2017-05-15 18:53:33 +0800

    Server generator update + more
    - Added sourcemod and meta support
    - Added map rotation generator
    - Added a LGSM article
    - Added tools: bind rotation generator & asci art maker

--- js/downloader_server.js
+++ js/downloader_server.js
@@ -176,6 +176,7 @@ jQuery(function($) {
 		var sv_maxcmdrate = $('#sv_maxcmdrate').val();
 		var sv_mincmdrate = $('#tf_use_fixed_weaponspreads').val();
 
+	
 		//get large piece of custom set
 		var bindings = $('#bindings').val();
 		var tftrueset = "";
@@ -188,7 +189,26 @@ jQuery(function($) {
 		if ($("#tftrue").is(':checked')) {
 			tftrueset = "//TFTrue settings \n\ntftrue_maxfov " + tftrue_maxfov + "\ntftrue_freezecam " + tftrue_freezecam + "\ntftrue_whitelist_id " + tftrue_whitelist_id + "\ntftrue_logs_apikey " + tftrue_logs_apikey + "\ntftrue_restorestats " + tftrue_restorestats + "\ntftrue_tv_autorecord " + tftrue_tv_autorecord + "\ntftrue_tv_demos_path " + tftrue_tv_demos_path + "\ntftrue_no_hats " + tftrue_no_hats + "\ntftrue_no_misc " + tftrue_no_misc + "\ntftrue_bunnyhop " + tftrue_bunnyhop + "\n";
 		}
+		
+		//fastDL
+		var sv_allowdownload = "";
+		var sv_downloadurl = "";
+		if ($("#sv_allowdownload").is(':checked')) {
+			var sv_allowdownload = "sv_allowdownload 1";
+			var sv_downloadurl = "sv_downloadurl "+ $('#sv_downloadurl').val();			
+		}
+		//maps
+		if ($("#map_rotation").is(':checked')) {
+			var selectedValues = $('#map_pickerino').val();
+			var mapsstr = "";
+			for(var i = 0; i < selectedValues.length; ++i){
+				 mapsstr += selectedValues[i]+"\r\n" ;
+				}
 
+			zip.file('cfg/mapcycle.txt', mapsstr);
+		}
+
+		
 		// find every checked item
 		$(this).find(":checked").each(function() {
 
@@ -399,12 +419,100 @@ jQuery(function($) {
 					binary: true
 				});
 			}
+			if (iswhat === "metamod") {
+				if (os == "linux") {
+				zippies.push('./addons/mmsource-1.10.7-linux.zip');
+				} else {
+				zippies.push('./addons/mmsource-1.10.7-windows.zip');					
+				}
+			}
+			if (iswhat === "sourcemod") {
+				if (os == "linux") {
+				zippies.push('./addons/sourcemod-1.8.0-linux.zip');
+				} else {
+				zippies.push('./addons/sourcemod-1.8.0-windows.zip');					
+				}
+				
+				var smadminsettings = [
+					'/**',
+					' * Made with CFG.TF - Server config generator.',
+					' * USE THIS SECTION TO DECLARE DETAILED ADMIN PROPERTIES.',
+					' *',
+					' * Each admin should have its own "Admin" section, followed by a name.',
+					' * The name does not have to be unique.',
+					' *',
+					' * Available properties: (Anything else is filtered as custom)',
+					' *      "auth"          - REQUIRED - Auth method to use.  Built-in methods are:',
+					' *                        "steam"  - Steam based authentication',
+					' *                        "name"   - Name based authentication',
+					' *                        "ip"	- IP based authentication',
+					' *                        Anything else is treated as custom.',
+					' *					 Note: Only one auth method is allowed per entry.',
+					' *',
+					' *      "identity"      - REQUIRED - Identification string, for example, a steamid or name.',
+					' *					 Note: Only one identity is allowed per entry.',
+					' *',
+					' *      "password"      - Optional password to require.',
+					' *      "group"         - Adds one group to the users group table.',
+					' *      "flags"         - Adds one or more flags to the users permissions.',
+					' *		"immunity"		- Sets the users immunity level (0 = no immunity).',
+					' *						  Immunity can be any value.  Admins with higher ',
+					' *						  values cannot be targetted.  See sm_immunity_mode ',
+					' *						  to tweak the rules.  Default value is 0.',
+					' *',
+					' * Example:',
+					'	"BAILOPAN"',
+					'	{',
+					'		"auth"			"steam"',
+					'		"identity"		"STEAM_0:1:16"',
+					'		"flags"			"abcdef"',
+					'	}',
+					' *',
+					' */',
+					'Admins',
+					'{',
+					'}'
+				];
+				var a2 = [''];
+				for (var k = 0; k <= admins_c; k++) {
+					if(typeof $('#sm_admin_alias'+k).val() !== 'undefined' ){ //checking for undefined that appears extra fields were added and removed
+					a2.push('',
+						'	"'+$('#sm_admin_alias'+k).val()+'"',
+							'		{',
+								'			"auth"			"steam"',
+								'			"identity"		"'+$('#sm_steamid'+k).val()+'"',
+								'			"flags"			"'+$('#sm_flags'+k).val()+'"',
+								'			"immunity"			"'+$('#sm_immunity'+k).val()+'"',
+							'		}',
+					''
+					);
+					}
+				}
+				smadminsettings.splice.apply(smadminsettings, [36, 0].concat(a2));
+				var smadminsettings_j = smadminsettings.join('\n');
+				zip.file('addons/sourcemod/configs/admins.cfg', smadminsettings_j);
+				
+			}
+			if (iswhat === "curl") {
+				if ($('#sourcemod').is(":checked")){
+					if (os == "linux") {
+					zippies.push('./addons/sm_plugins/curl_1.3.0.0_linux.zip');
+					} else {
+					zippies.push('./addons/sm_plugins/curl_1.3.0.0.zip');					
+					}
+				}
+			}
+			if (iswhat === "sm_plugins") {
+				if ($('#sourcemod').is(":checked")){
+					zippies.push(url);
+				}
+			}
 
 		});
 
 
 		//building and downloading static configs
-		zip.file('cfg/server.cfg', '//Server settings\nhostname "' + hostname + '"\nsv_password "' + sv_password + '"\nsv_lan ' + sv_lan + '\n\nrcon_password "' + rcon_password + '"\nsv_rcon_banpenalty ' + sv_rcon_banpenalty + '\nsv_rcon_minfailures ' + sv_rcon_minfailures + '\nsv_rcon_maxfailures ' + sv_rcon_maxfailures + '\nlog ' + log + '\nsv_logfile ' + sv_logfile + '\nsv_logecho ' + sv_logecho + '\nsv_logbans ' + sv_logbans + '\n\n\nsv_pure ' + sv_pure + '\nsv_pure_kick_clients ' + sv_pure_kick_clients + '\nsv_alltalk ' + sv_alltalk + '\nmp_allowspectators ' + mp_allowspectators + '\nmp_autoteambalance ' + mp_autoteambalance + '\nmp_teams_unbalance_limit ' + mp_teams_unbalance_limit + '\nmp_forcecamera ' + mp_forcecamera + '\nsv_allow_wait_command ' + sv_allow_wait_command + '\nsv_cheats ' + sv_cheats + '\nsv_pausable ' + sv_pausable + '\nmp_stalemate_enable ' + mp_stalemate_enable + '\nmp_stalemate_timelimit ' + mp_stalemate_timelimit + '\nmp_winlimit ' + mp_winlimit + '\nmp_timelimit ' + mp_timelimit + '\ntf_weapon_criticals ' + tf_weapon_criticals + '\ntf_use_fixed_weaponspreads ' + tf_use_fixed_weaponspreads + '\n\nsv_maxrate ' + sv_maxrate + '\nsv_minrate ' + sv_minrate + '\nsv_maxupdaterate ' + sv_maxupdaterate + '\nsv_minupdaterate ' + sv_minupdaterate + '\nsv_maxcmdrate ' + sv_maxcmdrate + '\nsv_mincmdrate ' + sv_mincmdrate + '\n\n\n' + tftrueset + '\n\n\n\n//Custom settings\n\n' + bindings + '\n\necho -----------------------------------------------------------\necho ----------------- Thanks for using CFG.TF -----------------\necho ---------Build your own custom config at cfg.tf------------\necho -----------------------------------------------------------'); //server
+		zip.file('cfg/server.cfg', '//Server settings\nhostname "' + hostname + '"\nsv_password "' + sv_password + '"\nsv_lan ' + sv_lan + '\n\nrcon_password "' + rcon_password + '"\nsv_rcon_banpenalty ' + sv_rcon_banpenalty + '\nsv_rcon_minfailures ' + sv_rcon_minfailures + '\nsv_rcon_maxfailures ' + sv_rcon_maxfailures + '\nlog ' + log + '\nsv_logfile ' + sv_logfile + '\nsv_logecho ' + sv_logecho + '\nsv_logbans ' + sv_logbans + '\n\n\nsv_pure ' + sv_pure + '\nsv_pure_kick_clients ' + sv_pure_kick_clients + '\nsv_alltalk ' + sv_alltalk + '\nmp_allowspectators ' + mp_allowspectators + '\nmp_autoteambalance ' + mp_autoteambalance + '\nmp_teams_unbalance_limit ' + mp_teams_unbalance_limit + '\nmp_forcecamera ' + mp_forcecamera + '\nsv_allow_wait_command ' + sv_allow_wait_command + '\nsv_cheats ' + sv_cheats + '\nsv_pausable ' + sv_pausable + '\nmp_stalemate_enable ' + mp_stalemate_enable + '\nmp_stalemate_timelimit ' + mp_stalemate_timelimit + '\nmp_winlimit ' + mp_winlimit + '\nmp_timelimit ' + mp_timelimit + '\ntf_weapon_criticals ' + tf_weapon_criticals + '\ntf_use_fixed_weaponspreads ' + tf_use_fixed_weaponspreads + '\n\nsv_maxrate ' + sv_maxrate + '\nsv_minrate ' + sv_minrate + '\nsv_maxupdaterate ' + sv_maxupdaterate + '\nsv_minupdaterate ' + sv_minupdaterate + '\nsv_maxcmdrate ' + sv_maxcmdrate + '\nsv_mincmdrate ' + sv_mincmdrate + '\n\n\n//Map settings\n\n'+sv_allowdownload+'\n'+sv_downloadurl+'\n\n\n' + tftrueset + '\n\n\n\n//Custom settings\n\n' + bindings + '\n\necho -----------------------------------------------------------\necho ----------------- Thanks for using CFG.TF -----------------\necho ---------Build your own custom config at cfg.tf------------\necho -----------------------------------------------------------'); //server
 
 
 			// when everything has been downloaded, we can trigger the dl
